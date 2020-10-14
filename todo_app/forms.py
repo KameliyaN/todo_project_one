671a4ddf1e4ada48804e9a269d1e905ec0f2b2e8
KameliyaN <72250856+KameliyaN@users.noreply.github.com>
2020-10-14 18:09:03 +0300

    forms classes

--- todo_app/forms.py
+++ todo_app/forms.py
@@ -1,2 +1,10 @@
 from django import forms
 
+
+class TodoForm(forms.Form):
+    title = forms.CharField(label='title', max_length=20)
+    description = forms.CharField(widget=forms.Textarea)
+
+
+class FormName(forms.Form):
+    name=forms.CharField(validators=[])
--- todo_app/views.py
+++ todo_app/views.py
@@ -2,12 +2,14 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from todo_app.forms import TodoForm
 from todo_app.models import Todo
 
 
 def index(request):
     todos = Todo.objects.all()
     context = {
-        'todos': todos
+        'todos': todos,
+
     }
     return render(request, 'todo_app/index.html', context=context)
