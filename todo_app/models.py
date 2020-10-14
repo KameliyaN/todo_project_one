fabd654bc08ab67589bb37172bfca3d4024720b2
KameliyaN <72250856+KameliyaN@users.noreply.github.com>
2020-10-14 12:23:19 +0300

    ste method added in miodels

--- todo_app/models.py
+++ todo_app/models.py
@@ -6,3 +6,6 @@ class Todo(models.Model):
     title = models.CharField(max_length=10)
     description = models.CharField(max_length=100)
     is_done = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'This is  {self.title} todo'
