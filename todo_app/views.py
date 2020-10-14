abd700536a99cf2d6d794bdea459f04ea24285a1
KameliyaN <72250856+KameliyaN@users.noreply.github.com>
2020-10-14 11:18:34 +0300

    add some logic to index.html

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,13 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-
 # Create your views here.
+from todo_app.models import Todo
+
+
 def index(request):
-    return render (request,'todo_app/index.html')
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos
+    }
+    return render(request, 'todo_app/index.html', context=context)
